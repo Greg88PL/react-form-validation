3cd167281a50f3853e68befe5351783021988f29
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-10-08 15:15:53 +0200

    add input validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,48 @@
+import React from "react";
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [nameTouched, setNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const inputNameisInvalid = !enteredNameIsValid && nameTouched;
+
+  const nameChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+    setEnteredName("");
+    setNameTouched("");
+  };
+
+  const inputNameBlurHandler = (event) => {
+    setNameTouched(true);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          onBlur={inputNameBlurHandler}
+          onChange={nameChangeHandler}
+          type="text"
+          id="name"
+          value={enteredName}
+        />
+        {inputNameisInvalid && (
+          <p className="error-text">Input field cannot be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
